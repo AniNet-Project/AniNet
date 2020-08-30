4d3d0fda8bdfd4fb1675d7a927c95f193a72749f
nanguage <nanguage@yahoo.com>
2020-08-30 18:02:14 +0800

    change analytics

--- aninet/src/Footer.tsx
+++ aninet/src/Footer.tsx
@@ -8,16 +8,12 @@ const Busuanzi = () => {
   if (isHome) {
     return (
       <div className="busuanzi">
-        <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
         <span id="busuanzi_container_site_pv"> 本站总访问量<span id="busuanzi_value_site_pv"></span>次 </span>
       </div>
     )
   } else {
     return (
-      <div className="busuanzi">
-        <script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>
-        <span id="busuanzi_container_page_pv"> 本页面访问量<span id="busuanzi_value_page_pv"></span>次 </span>
-      </div>
+      <></>
     )
   }
 }
